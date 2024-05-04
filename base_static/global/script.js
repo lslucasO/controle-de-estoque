fd26a4a7c68ee44bd703e878f8f40362f0fc116e
Lucas Santana Lemos <lslemos59@gmail.com>
2024-05-04 14:34:21 -0300

    #

--- base_static/global/script.js
+++ base_static/global/script.js
@@ -1,3 +1,13 @@
 function alertaUsuario(){
     return alert("Eae tu clicou aqui boy");
 }
+
+function appear(){
+    let menu_item = document.querySelector('.menu-item')
+    menu_item.style.width = '20%'
+}
+
+function disappear(){
+    let menu_item = document.querySelector('.menu-item')
+    menu_item.style.width = '0%'
+}
