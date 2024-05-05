602ccb4ae609357b4e9dcb7e15c195dc43154c04
Lucas Santana Lemos <lslemos59@gmail.com>
2024-05-05 16:26:15 -0300

    #

--- base_static/global/script.js
+++ base_static/global/script.js
@@ -2,12 +2,3 @@ function alertaUsuario(){
     return alert("Eae tu clicou aqui boy");
 }
 
-function appear(){
-    let menu_item = document.querySelector('.menu-item')
-    menu_item.style.width = '15%'
-}
-
-function disappear(){
-    let menu_item = document.querySelector('.menu-item')
-    menu_item.style.width = '0%'
-}
---
+++ stock/static/stock/js/menu-bar.js
@@ -0,0 +1,9 @@
+function appear(){
+    let menu_item = document.querySelector('.menu-item')
+    menu_item.style.width = '15%'
+}
+
+function disappear(){
+    let menu_item = document.querySelector('.menu-item')
+    menu_item.style.width = '0%'
+}
